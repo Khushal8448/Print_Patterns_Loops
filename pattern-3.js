56f88d5b3f18aa52e4c6b89cd11e022393efe3e5
Khushal Mali <khushalmali8448@gmail.com>
2024-06-15 14:13:51 +0530

    update

--- pattern-3.js
+++ pattern-3.js
@@ -17,7 +17,7 @@ Output:
 */
 
 function printPattern(n) {
-  for (let i = 1; i < n; i++) {
+  for (let i = 1; i <= n; i++) {
     let line = "";
     for (let j = 1; j <= i; j++) {
       line = line + " " + j;
